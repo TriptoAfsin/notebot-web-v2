7d4a8076a41b63ebda03f9dc31ba8383eeb6bd52
TriptoAfsin <AfsinTripto@gmail.com>
2024-11-14 19:38:28 +0600

    chore: add palestine solidarity

--- src/components/atoms/layout/footer.tsx
+++ src/components/atoms/layout/footer.tsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import { Text } from "../typography/text";
 import { Box } from "./box";
 
+const PALESTINE_FLAG = "/icons/palestine-flag.png";
+
 export default function Footer() {
   return (
     <footer className="text-gray-600 body-font dark:text-white">
@@ -11,13 +13,26 @@ export default function Footer() {
           <Text className="text-sm text-center text-gray-500 dark:text-white sm:text-left">
             © {new Date().getFullYear()} {APP_CONFIG.name}
           </Text>
+
           <Box
             as="span"
             className="inline-flex items-center justify-center mt-2 sm:ml-auto sm:mt-0 sm:justify-start"
           >
-            <Link to={APP_CONFIG.founder.web} target="_blank">
-              <Text>Made with ❤️ by {APP_CONFIG.founder.name}</Text>
-            </Link>
+            <Box className="flex flex-col items-center sm:items-start">
+              <Link to={APP_CONFIG.founder.web} target="_blank">
+                <Text>Made with ❤️ by {APP_CONFIG.founder.name}</Text>
+              </Link>
+              <Box className="flex items-center justify-center gap-2 mt-2">
+                <Text className="text-sm text-center text-gray-500 dark:text-white ">
+                  Solidarity with Palestine
+                </Text>
+                <img
+                  src={PALESTINE_FLAG}
+                  alt="Palestine Flag"
+                  className="w-6 h-6"
+                />
+              </Box>
+            </Box>
           </Box>
         </Box>
       </Box>
